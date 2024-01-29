83854d4ae459efd0b3b0e9bb54465b175eaf7bcb
andreivydra <andreivydra@yandex.ru>
2024-01-29 19:19:40 +0300

    angular

---
+++ my-app/dist/my-app/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-GYBUAH5Z.mjs";import"./chunk-VKRIJQWH.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
---
+++ my-app/src/environment/environment.ts
@@ -0,0 +1,20 @@
+export const backendURL: String = "http://localhost:20351/lab/api";
+
+export const yMin: number = -3;
+export const yMax: number = 5;
+export const rMin: number = 1;
+export const rMax: number = 4;
+
+// graph
+export const graphTop = 0;
+export const graphBottom = 10;
+export const graphLeft = 0;
+export const graphRight = 10;
+export const centerX = (graphRight + graphLeft) / 2
+export const centerY = (graphTop + graphBottom) / 2
+export const cellCount = 10
+export const cellSize = (graphRight - graphLeft) / cellCount
+export const lineWidth = 0.04;
+export const padding = 0.2;
+
+export const scale = 25;
